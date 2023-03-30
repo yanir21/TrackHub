e6c025024768369a04a36f2c6a4e4a383a7e330f
yanir21 <yanir334@gmail.com>
2023-03-30 23:59:11 +0300

    feat(several): Login & register pages

--- frontend/src/views/loginPage/loginPage.tsx
+++ frontend/src/views/loginPage/loginPage.tsx
@@ -1,7 +1,37 @@
-import React from "react";
+import React, { useState } from "react";
+import "./loginPage.scss";
+import SVG from "react-inlinesvg";
 
 const LoginPage = () => {
-  return <div className="login-page"></div>;
+  const [username, setUsername] = useState<string>("");
+  const [password, setPassword] = useState<string>("");
+
+  return (
+    <div className="login-page">
+      <SVG
+        className="clef-icon"
+        src={`${process.env.PUBLIC_URL}/icons/solclef.svg`}
+      />
+      <div className="title">Sign in to collaborate</div>
+      <form>
+        <div className="form-section">
+          <div className="field-title">Username</div>
+          <input type="text" className="field-input" />
+        </div>
+        <div className="form-section">
+          <div className="field-title">Password</div>
+          <input type="text" className="field-input" />
+        </div>
+        <div className="submit-container">
+          <input type="submit" value="Sign In" className="submit-button" />
+        </div>
+      </form>
+
+      <a className="register-link" href="/register">
+        Don't have an account yet?
+      </a>
+    </div>
+  );
 };
 
 export default LoginPage;
--- frontend/src/views/registerPage/registerPage.tsx
+++ frontend/src/views/registerPage/registerPage.tsx
@@ -1,7 +1,36 @@
 import React from "react";
+import "./registerPage.scss";
+import SVG from "react-inlinesvg";
 
 const RegisterPage = () => {
-  return <div className="register-page"></div>;
+  return (
+    <div className="register-page">
+      <SVG
+        className="clef-icon"
+        src={`${process.env.PUBLIC_URL}/icons/solclef.svg`}
+      />
+      <div className="title">
+        Register to start <label className="blue-label">collaborating</label>
+      </div>
+      <form>
+        <div className="form-section">
+          <div className="field-title">Username</div>
+          <input type="text" className="field-input" />
+        </div>
+        <div className="form-section">
+          <div className="field-title">Password</div>
+          <input type="text" className="field-input" />
+        </div>
+        <div className="submit-container">
+          <input type="submit" value="Register" className="submit-button" />
+        </div>
+      </form>
+
+      <a className="register-link" href="/login">
+        Already have an account?
+      </a>
+    </div>
+  );
 };
 
 export default RegisterPage;
