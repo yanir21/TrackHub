05c40734e5bc822a320931d21bccb681f9369e67
meyron1456 <meyron1456@gmail.com>
2023-04-02 19:13:00 +0300

    removing all dist folder changes

--- backend/dist/dto/create-user.dto.d.ts
+++
@@ -1,6 +0,0 @@
-export declare class CreateUserDto {
-    readonly username: string;
-    readonly password: number;
-    readonly displayname: number;
-    readonly imageurl: string;
-}
--- backend/dist/interface/user.interface.d.ts
+++
@@ -1,7 +0,0 @@
-import { Document } from 'mongoose';
-export interface IUser extends Document {
-    readonly username: string;
-    readonly password: string;
-    readonly displayname: string;
-    readonly imageurl: string;
-}
