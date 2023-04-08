7cfac65fa9af05790c6935341145fed7bc1f9b91
yanir21 <yanir334@gmail.com>
2023-04-08 18:41:14 +0300

    fix(several): prettier config file, auth guard file rename

--- backend/src/project/project.controller.ts
+++ backend/src/project/project.controller.ts
@@ -1,57 +1,57 @@
 import {
-  Controller,
-  Get,
-  Post,
-  Body,
-  Param,
-  Delete,
-  UseGuards,
-  Put,
+    Controller,
+    Get,
+    Post,
+    Body,
+    Param,
+    Delete,
+    UseGuards,
+    Put,
 } from '@nestjs/common';
 import { ProjectService } from './project.service';
 import { CreateProjectDto } from './dto/create-project.dto';
 import { UpdateProjectDto } from './dto/update-project.dto';
 import { User } from 'src/shared/decorators/user.decorator';
 import { UserService } from 'src/user/user.service';
-import { AuthGuard } from 'src/auth/auth.gurad';
+import { AuthGuard } from 'src/auth/auth.guard';
 
 @Controller('projects')
 @UseGuards(AuthGuard)
 export class ProjectController {
-  constructor(
-    private readonly projectService: ProjectService,
-    private readonly userService: UserService
-  ) {}
+    constructor(
+        private readonly projectService: ProjectService,
+        private readonly userService: UserService
+    ) {}
 
-  @Post()
-  async create(
-    @Body() createProjectDto: CreateProjectDto,
-    @User() username: string
-  ) {
-    const author = await this.userService.getUserByUsername(username);
-    return await this.projectService.create(createProjectDto, author._id);
-  }
+    @Post()
+    async create(
+        @Body() createProjectDto: CreateProjectDto,
+        @User() username: string
+    ) {
+        const author = await this.userService.getUserByUsername(username);
+        return await this.projectService.create(createProjectDto, author._id);
+    }
 
-  @Get()
-  async findAll() {
-    return await this.projectService.findAll();
-  }
+    @Get()
+    async findAll() {
+        return await this.projectService.findAll();
+    }
 
-  @Get(':id')
-  async findOne(@Param('id') id: string) {
-    return await this.projectService.findOne(id);
-  }
+    @Get(':id')
+    async findOne(@Param('id') id: string) {
+        return await this.projectService.findOne(id);
+    }
 
-  @Put(':id')
-  async update(
-    @Param('id') id: string,
-    @Body() updateProjectDto: UpdateProjectDto
-  ) {
-    return await this.projectService.update(id, updateProjectDto);
-  }
+    @Put(':id')
+    async update(
+        @Param('id') id: string,
+        @Body() updateProjectDto: UpdateProjectDto
+    ) {
+        return await this.projectService.update(id, updateProjectDto);
+    }
 
-  @Delete(':id')
-  async remove(@Param('id') id: string) {
-    return await this.projectService.remove(id);
-  }
+    @Delete(':id')
+    async remove(@Param('id') id: string) {
+        return await this.projectService.remove(id);
+    }
 }
