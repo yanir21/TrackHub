63d0117e19272e2954ee1e235132ab4dae398e59
Idan Arad <idanarad@Idans-MBP.lan>
2023-05-24 22:48:27 +0300

    added headers

--- frontend/src/services/http.ts
+++ frontend/src/services/http.ts
@@ -2,7 +2,7 @@ import axios from 'axios';
 import { getToken } from './auth';
 
 const http = axios.create({
-  baseURL: 'https://trackhubio.vercel.app/',
+  baseURL: 'http://trackhubio.vercel.app/',
 });
 
 http.interceptors.request.use(async (config) => {
